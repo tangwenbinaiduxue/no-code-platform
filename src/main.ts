23ce21990c34bfe27753fd6c2bd897f782f441da
唐文彬 <wenbin.tang@alsi.cn>
2024-07-09 22:27:51 +0800

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-console.log('1');
+
 const app = createApp(App)
-console.log('2');
 
 app.use(createPinia())
-console.log('3');
 app.use(router)
-console.log('4');
 
 app.mount('#app')
