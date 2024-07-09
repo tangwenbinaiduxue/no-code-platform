252894aa86415c7fa9dfb4b0bec2930a3dd09b03
唐文彬 <wenbin.tang@alsi.cn>
2024-07-09 22:01:07 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+console.log('1');
 const app = createApp(App)
+console.log('2');
 
 app.use(createPinia())
+console.log('3');
 app.use(router)
+console.log('4');
 
 app.mount('#app')
